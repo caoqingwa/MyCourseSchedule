13ad10a9dcb61531b799f6742353355e4323ac30
caoqingwa <1251972432@qq.com>
2026-07-23 14:40:33 +0800

    chore: bump version to 2.4 (versionCode 13)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.example.courseschedule"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "2.3"
+        versionCode = 13
+        versionName = "2.4"
     }
 
     signingConfigs {
